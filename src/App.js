99567783e34f6385d019f61f843ff3a70a829d16
Lam-Pham <lpham127@gmail.com>
2021-05-23 19:37:11 -0700

    set up json-server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import Spot from './components/Spot'
+import spotService from './services/spots'
 
-function App() {
+const App = () => {
+  const [spots, setSpots] = useState([])
+  const [newSpot, setNewSpot] = useState('')
+
+  useEffect(() => {
+    spotService
+      .getAll2()
+      .then(initialSpots => {
+      setSpots(initialSpots)
+    })
+  }, [])
+
+  const addSpot = (event) => {
+    event.preventDefault()
+    const spotObject = {
+      content: newSpot,
+      date: new Date().toISOString(),
+    }
+
+    spotService
+      .create(spotObject)
+        .then(returnedSpot => {
+        setSpots(spots.concat(returnedSpot))
+        setNewSpot('')
+      })
+  }
+
+  const handleSpotChange = (event) => {
+    console.log(event.target.value)
+    setNewSpot(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Spots</h1>
+      <ul>
+        {spots.map(spot => 
+            <Spot
+              key={spot.id}
+              spot={spot} 
+            />
+        )}
+      </ul>
+      <form onSubmit={addSpot}>
+        <input
+          value={newSpot}
+          onChange={handleSpotChange}
+        />
+        <button type="submit">save</button>
+      </form>  
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Spot.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+const Spot = ({ spot }) => {
+
+  return (
+    <li className='spot'>
+      {spot.content} 
+    </li>
+  )
+}
+
+export default Spot
