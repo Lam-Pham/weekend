34359f193fba8b0570e72dd6eb460f185612dc61
Lam-Pham <lpham127@gmail.com>
2021-05-24 15:30:48 -0700

    changed base url; added proxy for dev

--- src/services/spots.js
+++ src/services/spots.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://tranquil-sands-10756.herokuapp.com/api/spots'
+const baseUrl = '/api/spots'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
